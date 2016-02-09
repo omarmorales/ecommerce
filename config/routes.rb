43cec000b703eba5d1009e50eecde492ff213fd3
Omar Morales <saul090594gmail.com>
2016-02-08 22:40:28 -0600

    initial configuration and adding device gem

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  authenticated :user do
+    root 'welcome#index'
+  end
+
+  unauthenticated :user do
+    devise_scope :user do
+      root "welcome#unregistered", as: :unregistered_root
+    end
+  end      
+end
